cc6dd7eac124ee78959766a60b0b762692d6759d
AdamJamesNaylor <adamnaylor@gmail.com>
2016-07-04 23:34:08 +0100

    Added logo.

--- AJN.Jonesy/AJN.Jonesy.Business/QuestionUrlParser.cs
+++ AJN.Jonesy/AJN.Jonesy.Business/QuestionUrlParser.cs
@@ -5,18 +5,18 @@ namespace AJN.Jonesy.Business {
     using System.Text;
     using Model;
 
-    public class QuestionUrlParser : IQuestionUrlParser {
-        public string Generate(Question question) {
+    public static class QuestionUrlParser {
+        public static string Generate(Question question) {
 
             if (question == null)
                 return null;
 
             var result = question.Text.ToLower();
             result = Latinise(result);
-            return RemoveUnsupportedCharacters(result);
+            return string.Format("/questions/{0}/{1}", question.Id, RemoveUnsupportedCharacters(result));
         }
 
-        public string Latinise(string text) {
+        public static string Latinise(string text) {
             var normalizedString = text.Normalize(NormalizationForm.FormD);
             var stringBuilder = new StringBuilder();
 
@@ -29,7 +29,7 @@ namespace AJN.Jonesy.Business {
             return stringBuilder.ToString().Normalize(NormalizationForm.FormC);
         }
 
-        public string RemoveUnsupportedCharacters(string text) {
+        public static string RemoveUnsupportedCharacters(string text) {
             //taken from http://stackoverflow.com/questions/1856785/characters-allowed-in-a-url
             var unreserved = new[] {
                 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
